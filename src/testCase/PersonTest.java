996d4013c6de0889296e758331b227ba98ea40ab
margareta75 <margareta.xx@gmail.com>
2020-11-15 17:02:22 +0100

    Fixed code (spaces)

--- src/testCase/PersonTest.java
+++ src/testCase/PersonTest.java
@@ -16,41 +16,41 @@ class PersonTest {
 	// I chose method @BeforeEach, but it can also be @BeforeAll
 	@BeforeEach
 	public void setUpUser() {
-        firstName = "Gosia";
-        lastName = "Pick";
-        age = 45;
-    }
+		firstName = "Gosia";
+		lastName = "Pick";
+		age = 45;
+	}
 	
 	@Test
 	@DisplayName("Test first name")
 	public void testFirstName() {
-        assertEquals("Gosia", firstName, "First name is not Gosia");
-    }
+		assertEquals("Gosia", firstName, "First name is not Gosia");
+	}
 
 	@Test
 	@DisplayName("Test last name")
 	public void testLastName() {
-        assertEquals("Pick", lastName, "Last name is not Pick");
-    }
+		assertEquals("Pick", lastName, "Last name is not Pick");
+	}
 
 	@Test
 	@DisplayName("Test age")
 	public void testAge() {
-        assertEquals(45, age, "Age is not 45");
-    }
+		assertEquals(45, age, "Age is not 45");
+	}
 	
 	// Alternative solutions:	
 	@Test
 	@DisplayName("Test first name 2")
 	public void testFirstName2() {
-        assertSame("Gosia", firstName, "First name doesn't match");
-    }
+		assertSame("Gosia", firstName, "First name doesn't match");
+	}
 
 	@Test
 	@DisplayName("Test last name 2")
 	public void testLastName2() {
-        assertSame("Pick", lastName, "Last name doesn't match");
-    }
+		assertSame("Pick", lastName, "Last name doesn't match");
+	}
 	
 	@Test
 	@DisplayName("Test age 2")
